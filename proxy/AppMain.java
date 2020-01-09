e449381fd29477c64fcf824412be76d9976e2ba8
aiyan2 <aiyanma@gmail.com>
2020-01-08 16:29:00 -0800

    Update AppMain.java

--- proxy/AppMain.java
+++ proxy/AppMain.java
@@ -1,23 +1,51 @@
 package easycomm.proxy;
 
+import com.fortinet.devqa.Util;
+
+/**
+ * 2020-01-08: Added to support both side( left and right, or A, B) proxy
+ * 
+ * cmd example: At right side: root@johns-virtual-machine:/home/aiyan/jproxy#
+ * java -jar wirecat.jar 445 172.18.43.124 445 1 B
+ * 
+ * 
+ * 2019-12-24: added the RST at Proxy to support when ( at which round to send
+ * reset)
+ * 
+ * @author john
+ * 
+ *
+ */
 public class AppMain {
 
+	public static int localPort = 4450;
+	public static String remoteIP = "172.18.43.100";
+	public static int remotePort = 8080;
 
+	public static int midReset = 3; // the flow round to send the RST
 
-    public static void main(String args[]) {
+	public static boolean isLeft = false; // by default is Right (back side), used for 2 proxies .
 
+	public static void main(String args[]) {
 
-        String remoteHost = "172.18.43.124" ; //System.getProperty("remoteHost");
-        int remotePort = 445; //Integer.parseInt(System.getProperty("remotePort"));
-        int port = 4440; //Integer.parseInt(System.getProperty("port"));
+		if (args.length == 5) {
+			localPort = Integer.valueOf(args[0]);
+			remoteIP = (args[1]);
+			remotePort = new Integer(args[2]);
+			midReset = new Integer(args[3]);
 
-        LOGGER.info("Starting proxy server on port {} for remote {}:{}", port, remoteHost);
-        
-               	 
-        TcpProxy tcpProxy = new TcpProxy(remoteHost, remotePort, port);
-        tcpProxy.listen();
-    }
+			if (args[4] == "A")
+				isLeft = true;
 
+		} else {
+			LOGGER.info("Usage: {LocalPort} {RemoteIP} {RemotePORT} {midReset} {A/B left or right");
+			LOGGER.info("No IP and port defined, continue with: lPort " + localPort + " " + "rIP and Port " + remoteIP
+					+ ":" + remotePort + " midReset " + midReset);
+			// return;
+		}
 
-    
+		TcpProxy tcpProxy = new TcpProxy(remoteIP, remotePort, localPort);
+		tcpProxy.listen();
+	}
+
 }
